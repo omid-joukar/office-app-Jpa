d67717aae7b150d52398b459ad44e44eeb05918d
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-09-14 13:55:12 +0200

    testing criteria codes in officeTest

--- classicmodel/src/test/java/OfficesTest.java
+++ classicmodel/src/test/java/OfficesTest.java
@@ -1,6 +1,10 @@
 import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 
+import java.lang.annotation.Retention;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -38,5 +42,52 @@ public class OfficesTest {
             }
         }
     }
+    @Test
+    public void criteriaTest1(){
+        try(Session session = HiberConfig.getSession()){
+            List<Offices> officesList = session.createCriteria(Offices.class).add(Restrictions.like("officeCode" , "1")).list();
+            for (Offices offices : officesList){
+                System.out.println(offices);
+            }
+        }
+    }
+    @Test
+    public void criteriaTest2(){
+        try(Session session = HiberConfig.getSession()){
+            List<Offices> officesList = session.createCriteria(Offices.class)
+                    .add(Restrictions.between("officeCode" , "1","7"))
+                    .addOrder(Order.desc("officeCode")).list();
+            for (Offices offices : officesList){
+                System.out.println(offices);
+            }
+        }
+    }
+    @Test
+    public void criteriaTest3() {
+        Property officeCode = Property.forName("officeCode");
+        try (Session session = HiberConfig.getSession()) {
+            List<Offices> officesList = session.createCriteria(Offices.class)
+                    .add(Restrictions.disjunction().add(Restrictions.like(String.valueOf(officeCode), "1")))
+                    .list();
+            for (Offices offices : officesList) {
+                System.out.println(offices);
+            }
 
+        }
+    }
+    @Test
+    public void criteriaTest4() {
+        Property officeCode = Property.forName("officeCode");
+        try (Session session = HiberConfig.getSession()) {
+            List<Offices> officesList = session.createCriteria(Offices.class).createCriteria("employeesList" , "empl")
+                    .add(Restrictions.disjunction().add(Restrictions.like("empl.employeeNumber", 1002L)))
+                    .list();
+            for (Offices offices : officesList) {
+                for (Employees employees : offices.getEmployeesList()){
+                    System.out.println(employees);
+                };
+            }
+
+        }
+    }
 }
